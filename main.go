e5578ea2d0dc46d8b637b79486527c2809b50667
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-14 06:43:23 +0200

    Added more methods

--- main.go
+++ main.go
@@ -9,9 +9,25 @@ func home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from coffee land!"))
 }
 
+func coffeeView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("View coffee!"))
+}
+
+func coffeeDelete(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("View delete!"))
+}
+
+func coffeeAdd(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("View delete!"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/coffee/view", coffeeView)
+	mux.HandleFunc("/coffee/add", coffeeAdd)
+	mux.HandleFunc("/coffee/delete", coffeeDelete)
 
 	log.Print("Starting server on :4000")
 
