d2be2c475a2a1124ce1c4636ee813a615a1c54f9
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-14 06:37:40 +0200

    Added server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Hello from coffee land!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
