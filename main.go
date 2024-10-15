b44abc24e6755ae9aa96e52696a24b41fd37a5e7
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-15 07:09:32 +0200

    Refactir habndlers

--- main.go
+++ main.go
@@ -1,43 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Hello from coffee land!"))
-}
-
-func coffeeView(w http.ResponseWriter, r *http.Request){
-	w.Header().Add("Server", "Napkins")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte("View coffee!"))
-
-	
-}
-
-func coffeeDelete(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("View delete!"))
-
-	id, err := strconv.Atoi(r.PathValue("id"))
-
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	
-	msg := fmt.Sprintf("Delete a specific coffee with ID %d...", id)
-    w.Write([]byte(msg))
-
-	
-}
-
-func coffeeAdd(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Add coffee!"))
-}
 
 func main() {
 	mux := http.NewServeMux()
