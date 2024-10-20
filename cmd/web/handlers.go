006b9a4c9b6197b337fbf090cfea5508aa6cac8b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-20 21:34:08 +0200

    Fix typo

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -63,9 +63,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 
 	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/view.tmpl",
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/view.html",
 	}
 
 	ts, err := template.ParseFiles(files...)
