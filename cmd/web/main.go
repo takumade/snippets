fbdad443d0d4b2fa4921b7b0c0f3fb366210e020
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-11-01 05:30:16 +0200

    initi template cache and add it to application dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -35,9 +35,17 @@ func main() {
 
 	defer db.Close()
 
+	templateCache, err := newTemplateCache()
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
 		logger:   logger,
 		snippets: &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	logger.Info("starting server", "addr", *addr)
