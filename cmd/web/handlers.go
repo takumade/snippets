969ee2fc2589ecf8c449e7f1f9832e120b782554
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-11-01 05:52:21 +0200

    Do trial renderrs

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"snippetbox.takucoder.dev/internal/models"
+	"bytes"
 	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"snippetbox.takucoder.dev/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -95,13 +97,14 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return 
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-
-	err := ts.ExecuteTemplate(w, "base", data)
-
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
-	}	
+		return
+	}
 
+	w.WriteHeader(status)
+	buf.WriteTo(w)
 }
