7689f023f0348512b423d15716243fdf2df4ffd0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-11-11 05:59:44 +0200

    Add recover panic middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -29,6 +30,19 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 		)
 
 		app.logger.Info("Request started", "method", method, "uri", uri, "remote_addr", ip, "proto", proto)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler  {
+	return http.HandlerFunc(func(w http.ResponseWriter,  r *http.Request){
+		defer func(){
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
