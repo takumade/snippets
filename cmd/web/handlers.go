1d86e56334ac0e86e54601e53bbc2000737f8bed
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-11-11 06:25:09 +0200

    Get title, content and expires from form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -81,9 +81,22 @@ func (app *application) snippetAdd(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "Climb Mount Fuji, But slowly, slowly!"
-	expires := 7
+
+
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return 
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
