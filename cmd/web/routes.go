ae81085f84761b66a80ae7f55a03ec92282dab2b
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-12-04 07:50:03 +0200

    Fix route issue

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -25,7 +25,7 @@ func (app *application) routes() http.Handler {
 	// Auth
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
-	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
 	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
