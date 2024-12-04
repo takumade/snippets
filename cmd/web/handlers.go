a730bdd5cc7b8905ca66fb7525ad0971043487b6
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-12-04 07:45:55 +0200

    Added auth routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -133,3 +133,15 @@ func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Create a new user....")
 }
+
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a  login form to  the user...")
+}
+
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user....")
+}
+
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
+}
