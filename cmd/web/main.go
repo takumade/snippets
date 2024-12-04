b4648d154185ae3e73d047194f4c62cf51388674
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-12-04 07:16:46 +0200

    Add timeout settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -74,6 +74,9 @@ func main() {
 		Handler: app.routes(), 
 		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 		TLSConfig: tlsConfig,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
