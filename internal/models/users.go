f0a66fc3585c9ad28ed13590326dbe8c8b5ad8ed
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-02 22:05:12 +0200

    Correct syntax error

--- internal/models/users.go
+++ internal/models/users.go
@@ -24,15 +24,15 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
-	hhashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 
 	if err != nil {
 		return err
 	}
 
-	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES(?, ? ,?, UTC_TIMESAMP())`
+	stmt := `INSERT INTO users (name, email, hashed_password, created) VALUES (?, ?, ?, UTC_TIMESTAMP())`
 
-	_, err = m.DB.Exec(stmt, name, email, string(hhashedPassword))
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
 
 	if err != nil {
 		var mySQLError *mysql.MySQLError
