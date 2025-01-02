27c1af6b00215416115855c5f50ba9e63cdf58fe
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-02 20:56:41 +0200

    Correct email regex

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -7,7 +7,7 @@ import (
 	"unicode/utf8"
 )
 
-var EmailRx = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]")
+var EmailRx = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 
 type Validator struct {
