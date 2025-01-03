344af428bd0a36c1b545ac94e16f9bb9cb7b9c97
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-03 23:05:06 +0200

    Add Authentication handler

--- internal/models/users.go
+++ internal/models/users.go
@@ -23,6 +23,36 @@ type UserModel struct {
 	DB *sql.DB
 }
 
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int 
+	var hashedPassword []byte 
+
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows){
+			return 0, ErrorInvalidCredentials
+		}else{
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword){
+			return 0, ErrorInvalidCredentials
+		}else{
+			return 0, err
+		}
+	}
+
+	return id, nil
+}
+
 func (m *UserModel) Insert(name, email, password string) error {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
 
