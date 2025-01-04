ad9b3d27d80bda315e7488336120c534d6263607
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-04 13:33:07 +0200

    Add protected routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,8 +17,6 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("POST /snippet/add", dynamic.ThenFunc(app.snippetAdd))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 	mux.Handle("DELETE /snippet/delete/{id}", dynamic.ThenFunc(app.snippetDelete))
 
 
@@ -27,7 +25,16 @@ func (app *application) routes() http.Handler {
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
-	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
+	
+
+
+	protected := dynamic.Append(app.requireAuthentication)
+
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.userLogoutPost))
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.snippetCreatePost))
+
+
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
     return standard.Then(mux)
