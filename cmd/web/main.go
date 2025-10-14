cafd918c88a675dc8b44f930d358129d135ea71a
Takunda Madechangu <takundam@sonity.net>
2025-10-14 10:05:03 +0200

    Set same site to strict

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -57,6 +57,7 @@ func main() {
 	sessionManager.Store =  mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
 	sessionManager.Cookie.Secure = true
+	sessionManager.Cookie.SameSite = http.SameSiteStrictMode
 
 	app := &application{
 		logger:   logger,
