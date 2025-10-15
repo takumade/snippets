9a48dd9bca0b16912a476583c246aa757fb7b3c6
Takunda Madechangu <takundam@sonity.net>
2025-10-15 23:50:55 +0200

    fix he template home.html does not exist

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"html/template"
+	"io/fs"
 	"path/filepath"
 	"time"
 
 	"snippetbox.takucoder.dev/internal/models"
+	"snippetbox.takucoder.dev/ui"
 )
 
 type templateData struct {
@@ -30,7 +32,7 @@ var functions  = template.FuncMap{
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	pages, err := fs.Glob(ui.Files, "html/pages/*.html")
 
 	if err != nil {
 		return nil, err
@@ -38,22 +40,18 @@ func newTemplateCache() (map[string]*template.Template, error) {
 
 	for _, page := range pages {
 		name := filepath.Base(page)
+		
+		patterns := []string{
+			"html/base.html",
+			"html/partials/*.html",
+			page,
+		}
 
-		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
+		ts, err := template.New(name).Funcs(functions).ParseFS(ui.Files, patterns...)
 		if err != nil {
 			return nil, err
 		}
-
-		ts,err = ts.ParseGlob("./ui/html/partials/*.html")
-		if err != nil {
-			return nil, err
-		}
-
-		ts, err = ts.ParseFiles(page)
-		if err != nil {
-			return nil, err
-		}
-
+		
 		cache[name] = ts
 	}
 
