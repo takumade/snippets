6e2747544580442bc4d189e5d43ad84b7f37b9f7
Takunda Madechangu <takundam@sonity.net>
2025-10-19 21:20:23 +0200

    Loop through the tests

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -32,6 +32,17 @@ func TestHumanDate(t *testing.T){
 	}
 	
 	
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			hd := humanDate(tt.tm)
+			
+			if hd != tt.want{
+				t.Errorf("got %q; want %q", hd, tt.want)
+			}
+		})
+	}
+	
+	
 	tm := time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC)
 	hd := humanDate(tm)
 	
