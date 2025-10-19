e2abf90c02518e424b0c90df73da6eb8ceecb5f8
Takunda Madechangu <takundam@sonity.net>
2025-10-19 21:18:21 +0200

    Add tests slice

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -9,6 +9,29 @@ import (
 func TestHumanDate(t *testing.T){
 	
 	
+	tests := []struct {
+		name string 
+		tm time.Time
+		want string 
+	}{
+		{
+			name: "UTC",
+			tm: time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC),
+			want: "17 Mar 2024 at 10:15",
+		},
+		{
+			name: "Empty", 
+			tm: time.Time{},
+			want: "",
+		}, 
+		{
+			name: "CET",
+			tm: time.Date(2024, 3, 17, 10, 15, 0, 0, time.FixedZone("CET", 1*60*60)),
+			want: "17 Mar 2024, at 09:15",
+		},
+	}
+	
+	
 	tm := time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC)
 	hd := humanDate(tm)
 	
