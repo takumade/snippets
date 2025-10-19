4a8d0dc1245ca1880bbde06a658d990699ccdcf8
Takunda Madechangu <takundam@sonity.net>
2025-10-19 20:31:07 +0200

    test human date

---
+++ cmd/web/templates_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+
+func TestHumanDate(t *testing.T){
+	
+	
+	tm := time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC)
+	hd := humanDate(tm)
+	
+	if hd != "17 Mar 2024 at 10:15" {
+		t.Errorf("got %q; want %q", hd, "17 Mar 2024 at 10:15")
+	}
+}
