15cacea5374f3d109b8920b6648bd7de2af97736
Takunda Madechangu <takundam@sonity.net>
2025-10-19 21:20:53 +0200

    refactor; Remove stale code

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -41,12 +41,4 @@ func TestHumanDate(t *testing.T){
 			}
 		})
 	}
-	
-	
-	tm := time.Date(2024, 3, 17, 10, 15, 0, 0, time.UTC)
-	hd := humanDate(tm)
-	
-	if hd != "17 Mar 2024 at 10:15" {
-		t.Errorf("got %q; want %q", hd, "17 Mar 2024 at 10:15")
-	}
 }
