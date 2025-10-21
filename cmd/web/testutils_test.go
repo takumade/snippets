620ec91bd456463fe2b637ef69367ba87a567616
Takunda Madechangu <takundam@sonity.net>
2025-10-21 09:24:19 +0200

    Add cookie jar to testClient and disable cookie redirection

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -24,6 +25,19 @@ type testServer struct {
 
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+	
+	
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	ts.Client().Jar = jar
+	
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	
 	return &testServer{ts}
 }
 
