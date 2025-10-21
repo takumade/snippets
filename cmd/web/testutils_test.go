a5f70b61d8d0c8120cc1226cf2d8ed33f69122d6
Takunda Madechangu <takundam@sonity.net>
2025-10-21 09:16:04 +0200

    Add a helper get func for perfoming client get requests

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -27,3 +27,21 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 	return &testServer{ts}
 }
 
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string){
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+	
+	
+	return rs.StatusCode, rs.Header, string(body)
+}
+
