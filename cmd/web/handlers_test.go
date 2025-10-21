df6c1a8fd4aa2168ade66d1012396dad2a4c957f
Takunda Madechangu <takundam@sonity.net>
2025-10-21 09:19:06 +0200

    Implement the E2E test helpers

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -16,31 +16,14 @@ import (
 
 func TestPing(t *testing.T){
 	
-	app := &application{
-		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
-	}
+	app := newTestApplication(t)
 	
 	
-	ts := httptest.NewTLSServer(app.routes())
+	ts :=newTestServer(t, app.routes())
 	defer ts.Close()
 	
+	code, _, body := ts.get(t, "/ping")
 	
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
-	
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-	
-	
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	
-	if err != nil {
-		t.Fatal(err)
-	}
-	
-	body = bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
-	
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
